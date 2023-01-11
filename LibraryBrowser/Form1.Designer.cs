71141a405beaaac4079969c4a06aa4f5a9f79c44
John-Paul Smith <jps@johnpaulsmith.co.uk>
2023-01-11 13:03:14 +0000

    created simple directory browser, nothing special just a treeview

---
+++ LibraryBrowser/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace BigB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScan = new System.Windows.Forms.Button();
+            this.txtTargetFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(420, 26);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 0;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // txtTargetFolder
+            // 
+            this.txtTargetFolder.Location = new System.Drawing.Point(94, 28);
+            this.txtTargetFolder.Name = "txtTargetFolder";
+            this.txtTargetFolder.Size = new System.Drawing.Size(320, 20);
+            this.txtTargetFolder.TabIndex = 1;
+            this.txtTargetFolder.Text = "\\\\NAS\\Assets\\_Image Library\\Dodgy Stock";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Library Root";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 54);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(243, 361);
+            this.treeView1.TabIndex = 3;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTargetFolder);
+            this.Controls.Add(this.btnScan);
+            this.Name = "Form1";
+            this.Text = "Library Browser - DAMN";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.TextBox txtTargetFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
---
+++ LibraryBrowser/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BigB
+{
+    public partial class Form1 : Form
+    {
+
+        int directoryCount =0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            string targetFolder = txtTargetFolder.Text;
+
+
+            btnScan.Enabled=false;
+            treeView1.Enabled=btnScan.Enabled ;
+            txtTargetFolder.Enabled= btnScan.Enabled ;
+            this.Cursor = Cursors.WaitCursor;
+         Application.DoEvents();
+
+            ScanDirectory(targetFolder);
+
+ 
+            btnScan.Enabled=true;
+            treeView1.Enabled=btnScan.Enabled ;
+            txtTargetFolder.Enabled= btnScan.Enabled ;
+            this.Cursor = Cursors.Default;
+             Application.DoEvents();   
+
+
+            MessageBox.Show("Completed");
+        }
+
+        private void ScanDirectory(string targetFolder)
+        {
+
+            // as we have no root nodes, create one for the targetFolder:
+                  string text =System.IO.Path.GetFileName( targetFolder);
+                string key = targetFolder;       
+            TreeNode libraryNode = treeView1.Nodes.Add(text, key);
+
+            ScanDirectory(targetFolder, libraryNode);
+
+            libraryNode.Expand();
+        }
+
+
+            private void ScanDirectory(string targetFolder, TreeNode rootNode)
+            {
+           // scan the defined folder for folders:
+
+            toolStripStatusLabel2.Text=$"Scanning '{targetFolder}'...";
+            Application.DoEvents();
+
+            foreach (string directory in System.IO.Directory.EnumerateDirectories(targetFolder))
+            {
+                string text =System.IO.Path.GetFileName( directory);
+                string key = directory;
+               TreeNode newNode= rootNode.Nodes.Add(key, text);
+                directoryCount++;
+
+ 
+
+                if(System.IO.Directory.GetDirectories(directory).Length > 0)
+                {
+  ScanDirectory(directory , newNode);
+                }
+                    toolStripStatusLabel1.Text=$"{directoryCount} directories scanned";
+                  Application.DoEvents();          
+            }
+            }
+        
+    }
+}
